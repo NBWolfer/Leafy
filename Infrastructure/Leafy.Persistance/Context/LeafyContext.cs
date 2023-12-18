20110c4f8140a9d87d83e4dee64ab62ff2360899
Berke Erkan Doğan <berkan.dogan@gazi.edu.tr>
2023-12-18 16:12:10 +0300

    sq

--- Infrastructure/Leafy.Persistance/Context/LeafyContext.cs
+++ Infrastructure/Leafy.Persistance/Context/LeafyContext.cs
@@ -7,10 +7,11 @@ namespace Leafy.Persistance.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=MAHMUT-ENES/MSSQL22_S1;initial Catalog=Leafy;integrated Security=true;TrustServerCertificate= true");
+            optionsBuilder.UseSqlServer(@"Server=MAHMUT-ENES\MSSQL22_S1;initial Catalog=Leafy;integrated Security=true;TrustServerCertificate= true");
         }
 
         public DbSet<Plant> Plants { get; set; }
         public DbSet<Disease> Diseases { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }

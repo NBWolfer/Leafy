5e9d5a512099e4172dc2e724e87ec15ee8c3238e
NBWolfer <32mahmutenes32@gmail.com>
2023-12-19 17:21:14 +0300

    Password hashing added.

--- Core/Leafy.Application/Features/Handlers/UserHandlers/CreateUserCommandHandler.cs
+++ Core/Leafy.Application/Features/Handlers/UserHandlers/CreateUserCommandHandler.cs
@@ -9,22 +9,22 @@ namespace Leafy.Application.Features.Handlers.UserHandlers
 {
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand>
     {
-        private readonly IRepository<User> _repository;
+        private readonly IUserRepository _repository;
 
-        public CreateUserCommandHandler(IRepository<User> repository)
+        public CreateUserCommandHandler(IUserRepository repository)
         {
             _repository = repository;
         }
 
         public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-
+            string hashedPassword = _repository.HashPassword(request.Password, IUserRepository.SecretKey, IUserRepository.Pepper, IUserRepository.Iteration);
 
             await _repository.CreateAsync(new User
             {
                 Name = request.Name,
                 Email = request.Email,
-                Password = request.Password
+                Password = hashedPassword
             });
         }
     }
---
+++ Infrastructure/Leafy.Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using Leafy.Application.Interfaces;
+using Leafy.Domain.Entities;
+using Leafy.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leafy.Persistance.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly LeafyContext _context;
+
+        public UserRepository(LeafyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            _context.Set<User>().Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _context.Set<User>().ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Set<User>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync(User entity)
+        {
+            _context.Set<User>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            _context.Set<User>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public string HashPassword(string password, string salt, string pepper, int iteration)
+        {
+            if(iteration <= 0 ) return password;
+            using var sha256 = SHA256.Create();
+            var passwordWithSalt = password + salt + pepper;
+            var bytehash = sha256.ComputeHash(Encoding.UTF8.GetBytes(passwordWithSalt));
+            var hash = Convert.ToBase64String(bytehash);
+            return HashPassword(hash, salt, pepper, iteration - 1);
+        }
+
+
+    }
+}

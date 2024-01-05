2f7b62313d8ccbcdbdb19666f85102e8248c7265
Berke Erkan Doğan <berkan.dogan@gazi.edu.tr>
2024-01-05 19:37:56 +0300

    Users test için değiştirldi.

--- leafy.client/src/Pages/AboutUs.tsx
+++ leafy.client/src/Pages/AboutUs.tsx
@@ -1,6 +1,7 @@
 import '../assets/aboutus.css';
 import rs from '../Images/cute-happy-funny-monstera-plant-pot-vector-cartoon-character-illustration-design-isolated_92289-1278.avif';
 import r from '../Images/personal-growth.png'
+import berke from '../Images/dasda.png'
 function AboutUs() {
     return (
         <>
